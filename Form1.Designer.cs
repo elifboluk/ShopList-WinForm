0309f01e1965c13c52c57089c0668d120e448c3d
elifboluk <elifbolukk@gmail.com>
2022-09-22 13:00:03 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ShopList
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lstBox = new DevExpress.XtraEditors.CheckedListBoxControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.btnAdd = new DevExpress.XtraEditors.SimpleButton();
+            this.btnUpdate = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDelete = new DevExpress.XtraEditors.SimpleButton();
+            this.btnComplete = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.lstBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lstBox.Appearance.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.lstBox.Appearance.Options.UseBackColor = true;
+            this.lstBox.Appearance.Options.UseFont = true;
+            this.lstBox.Appearance.Options.UseForeColor = true;
+            this.lstBox.Appearance.Options.UseTextOptions = true;
+            this.lstBox.AppearanceDisabled.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstBox.AppearanceDisabled.Options.UseFont = true;
+            this.lstBox.AppearanceSelected.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstBox.AppearanceSelected.Options.UseFont = true;
+            this.lstBox.CheckMode = DevExpress.XtraEditors.CheckMode.Single;
+            this.lstBox.ContextButtonOptions.ItemCursor = System.Windows.Forms.Cursors.Hand;
+            this.lstBox.ContextButtonOptions.PanelCursor = System.Windows.Forms.Cursors.Arrow;
+            this.lstBox.Location = new System.Drawing.Point(12, 12);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(260, 374);
+            this.lstBox.TabIndex = 0;
+            this.lstBox.ItemCheck += new DevExpress.XtraEditors.Controls.ItemCheckEventHandler(this.lstBox_ItemCheck);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(13, 393);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(182, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(202, 393);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(70, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(13, 420);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(105, 420);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.Location = new System.Drawing.Point(192, 420);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(80, 23);
+            this.btnComplete.TabIndex = 5;
+            this.btnComplete.Text = "Clear All";
+            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(284, 447);
+            this.Controls.Add(this.btnComplete);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.lstBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shop List";
+            ((System.ComponentModel.ISupportInitialize)(this.lstBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.CheckedListBoxControl lstBox;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton btnAdd;
+        private DevExpress.XtraEditors.SimpleButton btnUpdate;
+        private DevExpress.XtraEditors.SimpleButton btnDelete;
+        private DevExpress.XtraEditors.SimpleButton btnComplete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.Utils.Extensions;
+using DevExpress.XtraPrinting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopList
+{
+    public partial class frmMain : Form
+    {
+        string yol = "ShopList.Txt";
+        
+        
+        public frmMain()
+        {
+            if (!File.Exists(yol))
+            { File.Create(yol); }
+            
+            InitializeComponent();
+            string[] former = File.ReadAllLines(yol).ToArray();
+            
+            foreach (string s in former)
+            {
+                lstBox.Items.Add(s);
+            }
+            
+            
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Add(textEdit1.Text);
+            string[] str = new string[lstBox.Items.Count];
+            for(int i=0; i<lstBox.Items.Count;i++)
+            {
+                str[i] = lstBox.Items[i].ToString();
+                
+            }
+            File.WriteAllLines(yol, str);
+            textEdit1.Text = string.Empty;
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Remove(lstBox.SelectedItem);
+            string[] str = new string[lstBox.Items.Count];
+            for (int i = 0; i < lstBox.Items.Count; i++)
+            {
+                str[i] = lstBox.Items[i].ToString();
+
+            }
+            File.WriteAllLines(yol, str);
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            lstBox.SetItemValue(textEdit1.Text,lstBox.SelectedIndex);
+            string[] str = new string[lstBox.Items.Count];
+            for (int i = 0; i < lstBox.Items.Count; i++)
+            {
+                str[i] = lstBox.Items[i].ToString();
+
+            }
+            File.WriteAllLines(yol, str);
+        }
+
+        private void btnComplete_Click(object sender, EventArgs e)
+        {
+            int s = lstBox.ItemCount;
+            for(int i=0; i<s; i++)
+            {
+               lstBox.Items.RemoveAt(0);
+            }
+            File.WriteAllText(yol, String.Empty);
+        }
+
+        private void lstBox_ItemCheck(object sender, DevExpress.XtraEditors.Controls.ItemCheckEventArgs e)
+        {
+            
+        }
+    }
+
+   
+}
